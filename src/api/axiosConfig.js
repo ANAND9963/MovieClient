81a92b5fe9a9dffe73b3e7de453af5fa89d544fd
Anand <yannamanenianandkumar@gmail.com>
2024-11-17 20:34:27 -0500

    Completed the frontend

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL:'http://localhost:8080',
-    headers:{"ngrok-skip-browser-warning":"true"}
+
+const apiClient = axios.create({
+    baseURL: `${process.env.REACT_APP_API_URL}`,  // Dynamic base URL from environment variable
+    headers: {
+        "ngrok-skip-browser-warning": "true"  // Custom header to skip ngrok warning
+    }
 });
+
+export default apiClient;
