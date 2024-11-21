2e43a8d62addc12ad190ba84906f17bbc7738d49
Anand <yannamanenianandkumar@gmail.com>
2024-11-21 09:42:13 -0500

    Bugs

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -14,15 +14,15 @@ console.log("Spring express" , process.env.REACT_APP_API_URL);
 export const apiClient = axios.create({
     baseURL: `${SPRING_CONNECTION_BACKEND}`,  // Dynamic base URL from environment variable
 
-    headers: {
-        "ngrok-skip-browser-warning": "true"  // Custom header to skip ngrok warning
-    }
+    // headers: {
+    //     "ngrok-skip-browser-warning": "true"  // Custom header to skip ngrok warning
+    // }
 });
 export const apiClient2 = axios.create({
     baseURL: `${NODE_EXPRESS_BACKEND}`,  // Dynamic base URL from environment variable
 
-    headers: {
-        "ngrok-skip-browser-warning": "true"  // Custom header to skip ngrok warning
-    }
+    // headers: {
+    //     "ngrok-skip-browser-warning": "true"  // Custom header to skip ngrok warning
+    // }
 });
 
